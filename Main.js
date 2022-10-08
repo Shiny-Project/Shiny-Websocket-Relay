f3f3f02001a3f84362874c66d6e9a89cb624ce89
Eridanus Sora <sora@sound.moe>
2022-10-08 11:22:36 +0800

    allow cors

--- Main.js
+++ Main.js
@@ -1,30 +1,33 @@
-const app = require('http').createServer(handler);
-const io = require('socket.io')(app);
-const fs = require('fs');
+const app = require("http").createServer(handler);
+const io = require("socket.io")(app, {
+    cors: {
+        origin: true,
+    },
+});
+const fs = require("fs");
 
 const port = 3737; // 监听端口设置
-const serverAddress = '::ffff:106.186.22.246';
+const serverAddress = "::ffff:106.186.22.246";
 
 app.listen(port);
 
-function handler (req, res) {
+function handler(req, res) {}
 
-}
-
-io.on('connection', function (socket) {
-    console.log('Client connected. From: ' + socket.handshake.address);
+io.on("connection", function (socket) {
+    console.log("Client connected. From: " + socket.handshake.address);
     // if (socket.handshake.address !== serverAddress){
     //     return;
     // }
-    socket.on('event', function (data) {
-        console.log('Message received: ' + data + 'From: ' + socket.handshake.address);
-        socket.broadcast.emit('event', data);
-        console.log('Message broadcasted.')
+    socket.on("event", function (data) {
+        console.log(
+            "Message received: " + data + "From: " + socket.handshake.address
+        );
+        socket.broadcast.emit("event", data);
+        console.log("Message broadcasted.");
     });
-    socket.on('ping', function(socketId, message) {
-        socket.to(socketId).emit('pong', {});
-    })
+    socket.on("ping", function (socketId, message) {
+        socket.to(socketId).emit("pong", {});
+    });
 });
 
-console.log('Listening on localhost:' + port);
-
+console.log("Listening on localhost:" + port);
